d02753c6979127ac5d1470c874616c295c761fad
Jack Stettner <jack@stet.com>
2020-11-09 21:54:14 -0500

    Added more game logic

--- tetris/tetris.py
+++ tetris/tetris.py
@@ -3,6 +3,7 @@ import numpy as np
 
 HEIGHT = 40
 WIDTH = 10
+ROW_VALUE = 100
 
 class Tetris:
     board = []
@@ -13,7 +14,7 @@ class Tetris:
         self.board = np.array(np.zeros((HEIGHT, WIDTH)))
 
     def is_out_of_bounds(self, x, y):
-        return y >= len(board) or x >= len(board[0]) or x < 0
+        return y >= len(self.board) or x >= len(self.board[0]) or x < 0
 
     def is_intersecting(self):
         for i in range(4):
@@ -36,6 +37,32 @@ class Tetris:
                 if idx in self.tet.current():
                     self.board[self.tet.y + j][self.tet.x + i] = 1
 
-    # def clear_rows(self):
+    def shift_down(from_row):
+        for row in reversed(range(1, from_row)):
+            for col in range(len(board[0])):
+                self.board[row][col] = self.board[row - 1][col]
+
+        for col in range(len(board[0])):
+            self.board[0][col] = 0
+
+    def clear_rows(self):
+        rows_cleared = 0
+        for row in range(len(board)):
+            gap = False
+            for col in range(len(board[0])):
+                if self.board[row][col] == 0:
+                    gap = True
+
+            # If row complete
+            if gap == False:
+                for col in range(len(board[0])):
+                    self.board[row][col] = 0
+                self.shift_down(row)
+                rows_cleared += 1
+
+        self.score += ROW_VALUE * rows_cleared**2
+
+
+                
 
 
