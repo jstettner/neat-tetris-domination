7cf565d78f00db577eb0d4609782d594877b4e16
Jack Stettner <jack@stet.com>
2020-12-11 18:16:17 -0500

    factor score into fitness

--- src/dominator.py
+++ src/dominator.py
@@ -77,7 +77,7 @@ def eval_genome_top_four(genome, config):
         # scores.append(game.tetris.turns + game.tetris.score)
 
     # return np.average(scores)
-    return game.tetris.turns
+    return game.tetris.turns + game.tetris.score
 
 def train(generations = 100, checkpt = None):
     local_dir = os.path.dirname(__file__)
